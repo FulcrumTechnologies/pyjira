0dd4c8ce71284c0eec386bda7c00d5bc14b5a229
calebh93 <hawkins.caleb93@gmail.com>
2016-04-26 12:03:50 -0700

    Added two more functions

--- pyjira/actions.py
+++ pyjira/actions.py
@@ -2,6 +2,26 @@ import pyjira.api as _api
 import json as _json
 
 
-def get_summary(id):
-    """Get summary of a ticket."""
+def get_issue(id):
+    """Get issue and its details.
+    Parameters:
+    - id: id of an issue.
+    """
     return _api.rest("/issue/" + str(id))
+
+
+def get_all_fields():
+    """Get all existing fields."""
+    return _api.rest("/field")
+
+
+def get_field(id):
+    """Get field and its details.
+    Parameters:
+    - id: id of a field.
+    """
+    fields = _json.loads(get_all_fields())
+    for f in fields:
+        if (f["id"] == str(id) or
+            f["id"].replace("customfield_", "") == str(id)):
+            return _json.dumps(f)
